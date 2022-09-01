df0da6990978aaaa409943d9dc1a0876c868f213
Tomas Hehejik <thehejik@suse.com>
2022-09-01 11:43:08 +0000

    Basic extraEnv handling for epinio-server deployment

--- acceptance/install/scenario3_test.go
+++ acceptance/install/scenario3_test.go
@@ -27,6 +27,8 @@ var _ = Describe("<Scenario3> RKE, Private CA, Configuration, on External Regist
 		rangeIP           string
 		domain            string
 		domainIP          string
+		extraEnvName      string
+		extraEnvValue     string
 		// testenv.New is not needed for VerifyAppConfigurationBound helper :shrug:
 		env          testenv.EpinioEnv
 		localpathURL = "https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.20/deploy/local-path-storage.yaml"
@@ -61,6 +63,11 @@ var _ = Describe("<Scenario3> RKE, Private CA, Configuration, on External Regist
 			"--set", "global.registryNamespace=splatform",
 		}
 
+		extraEnvName, name_exists := os.LookupEnv("EXTRAENV_NAME")
+		extraEnvValue, value_exists := os.LookupEnv("EXTRAENV_VALUE")
+		if name_exists && value_exists {
+			flags = append(flags, "--set", "extraEnv[0].name=" + extraEnvName, "--set-string", "extraEnv[0].value=" + extraEnvValue)
+		}
 	})
 
 	AfterEach(func() {
